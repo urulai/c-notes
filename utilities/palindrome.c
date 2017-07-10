df2638a3aea36f326e1deb0670baba242681a068
urulai <subramanian.narasimhan@gmail.com>
2017-07-10 11:37:47 +0800

    Added zero character check and a loop

--- utilities/palindrome.c
+++ utilities/palindrome.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  char buffer[1024] = {0};
-  scanf("%s", buffer);
+	char buffer[1024] = {0};
+	if(argc < 2)
+		strcpy(buffer, "");  //Empty string.
+	else
+		strcpy(buffer, argv[1]);
+	
+	char user[2];
 
-  unsigned int N = strlen(buffer);
+	do 
+	{
+		unsigned int N = strlen(buffer);
+		int i = 0, l = N-1;
 
-  int i = 0, l = N-1;
-  while((i < N/2) && buffer[i++] == buffer[l--])
-  ;
-  if(i == N/2)
-   printf("palindrome\n");
-  else
-   printf("Try again.\n");
-  
-  return 0;
+		while((i < N/2) && buffer[i++] == buffer[l--])
+			;
+
+		if(i != 0 && i == N/2) // added i! = 0 condition for empty string
+			printf("'%s' is palindrome\n", buffer);
+		else
+			printf("'%s' is not a palindrome. Try again.\n", buffer);
+
+		sleep(1);
+		
+		printf("Do you want to continue? (y/n) => ");
+		scanf("%s", user);
+		
+		if(user[0]!= 'y')
+			break;
+		else
+		{
+			memset(buffer, 0, sizeof buffer);
+			printf("Enter some string for checking: ");
+
+			scanf("%s", buffer);
+		}
+	} while(user[0] == 'y' || user[0] == 'Y');
+
+	return 0;
 }
